451e35d786c4c71f23b39d56abdebe389ecc8c23
David Giesberg <git@davidgiesberg.net>
2016-04-10 17:20:22 -0500

    Version bump motdserver -> 2.0.0

--- cookbooks/motdserver/metadata.rb
+++ cookbooks/motdserver/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures motdserver'
 long_description 'Installs/Configures motdserver'
-version '1.0.0'
+version '2.0.0'
 
 depends 'example', '~> 3.0.0'
 depends 'nginx', '= 2.7.6'
